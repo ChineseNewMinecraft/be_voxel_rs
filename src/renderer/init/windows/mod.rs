8e94a32b094b68e25dbfe3d99deaabe9d88a6172
LemonHX <runzhe2001@hotmail.com>
2020-06-28 04:08:06 +0200

    🍋 test vulkan api

---
+++ src/renderer/init/windows/mod.rs
@@ -0,0 +1,42 @@
+extern crate winapi;
+use ash::extensions::khr::Win32Surface;
+pub use ash::version::{DeviceV1_0, EntryV1_0, InstanceV1_0};
+use ash::vk;
+use std::default::Default;
+use ash::extensions::{
+    ext::DebugUtils,
+    khr::Surface,
+};
+use std::ffi::c_void;
+
+
+pub unsafe fn create_surface<E: EntryV1_0, I: InstanceV1_0>(
+    entry: &E,
+    instance: &I,
+    window: &winit::window::Window,
+) -> Result<vk::SurfaceKHR, vk::Result> {
+    use std::ptr;
+    use winapi::shared::windef::HWND;
+    use winapi::um::libloaderapi::GetModuleHandleW;
+    use winit::platform::windows::WindowExtWindows;
+
+    let hwnd = window.hwnd() as HWND;
+    let hinstance = GetModuleHandleW(ptr::null()) as *const c_void;
+    let win32_create_info = vk::Win32SurfaceCreateInfoKHR {
+        s_type: vk::StructureType::WIN32_SURFACE_CREATE_INFO_KHR,
+        p_next: ptr::null(),
+        flags: Default::default(),
+        hinstance,
+        hwnd: hwnd as *const c_void,
+    };
+    let win32_surface_loader = Win32Surface::new(entry, instance);
+    win32_surface_loader.create_win32_surface(&win32_create_info, None)
+}
+
+pub fn extension_names() -> Vec<*const i8> {
+    vec![
+        Surface::name().as_ptr(),
+        Win32Surface::name().as_ptr(),
+        DebugUtils::name().as_ptr(),
+    ]
+}
